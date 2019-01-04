c08d7ffb08e043a7934e27dd77c1385c9cae5276
Jeni Tennison <jeni@jenitennison.com>
2019-01-04 08:29:41 +0000

    update API keys to work with new domain

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@
 /* global URL */
 
 // Client ID and API key from the Developer Console
-var CLIENT_ID = '580782451843-b14d0fq1lu5a6ag1qldg4imfbjb00dtg.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyBJgCluChiRKeB21y2zqsxdELyZrxMmmGM';
+var CLIENT_ID = '580782451843-c3hia6gsl157hihk25tl4cqoil3gpi3u.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyAXqF7CUsCRncsTULLaHqNBtTnrtT-e1ic';
 
 // Array of API discovery doc URLs for APIs used by the quickstart
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
